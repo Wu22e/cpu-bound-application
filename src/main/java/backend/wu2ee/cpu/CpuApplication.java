8408e924a1f24228bc761fd232f35414ab15e58d
wu2ee <gulb1602@gmail.com>
2022-06-16 23:13:42 +0900

    git init
    - Hash 연산 10 만번 하는 컨트롤러 생성

---
+++ src/main/java/backend/wu2ee/cpu/CpuApplication.java
@@ -0,0 +1,13 @@
+package backend.wu2ee.cpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/wu2ee/cpu/CpuApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.wu2ee.cpu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
